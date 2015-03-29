58cbf0ed69a5508eac4f5e8ee0f99b3ed99c4276
Eric Sayler <esayler@colgate.edu>
2015-03-28 21:48:48 -0400

    add omniauth to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,29 @@
 class SessionsController < ApplicationController
-  def create
-    raise request.env["omniauth.auth"].to_yaml
+
+  def new
+    # signin route
+    redirect_to '/auth/twitter'
   end
+
+  def create
+    auth = request.env["omniauth.auth"]
+    # check db if user's data is there. if it's not there, create new user record
+    user = User.where(:provider => auth['provider'],
+                      :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
+    reset_session
+    session[:user_id] = user.id
+    redirect_to root_url, :notice => 'Signed in!'
+  end
+
+  def destroy
+    # signout route
+    reset_session
+    redirect_to root_url, :notice => 'Signed out!'
+  end
+
+  def failure
+    # authentication error (/auth/failure route)
+    redirect_to root_url, :alert => "Authentication error: #{params[:message].humanize}"
+  end
+
 end
