85c459e9d070450a3a49a9aa777607951b740004
Eric Sayler <esayler@colgate.edu>
2015-03-28 21:36:45 -0400

    add omniauth user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      if auth['info']
+        user.name = auth['info']['name'] || ""
+        user.email = auth['info']['email'] || ""
+        #user.avatar = auth['info']['image']
+      end
+    end
+  end
 end
