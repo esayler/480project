3a4d2ad64ab870396b30725adc52361a94d363a9
Eric Sayler <esayler@colgate.edu>
2015-03-31 18:23:40 -0400

    add simple visitor controller test

--- spec/controllers/visitors_controller_spec.rb
+++ spec/controllers/visitors_controller_spec.rb
@@ -1,5 +1,12 @@
-require 'rails_helper'
+describe VisitorsController do
 
-RSpec.describe VisitorsController, :type => :controller do
+  describe 'GET #index' do
+
+    it "renders the :index template" do
+      get :index
+      expect(response).to render_template :index
+    end
+
+  end
 
 end
