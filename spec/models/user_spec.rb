2843a0d101deec2f3f9d241ebff49c4323982e85
Eric Sayler <esayler@colgate.edu>
2015-03-31 19:06:06 -0400

    Add factory_girl shortcuts allows for the omission of 'FactoryGirl.' in front of 'create' and 'build' method calls within rspec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ describe User do
   describe "when email is non-empty" do
 
     it "should be valid" do
-      user = FactoryGirl.build(:user)
+      user = build(:user)
       expect(user).to be_valid
     end
 
@@ -12,7 +12,7 @@ describe User do
   describe "when email is blank" do
 
     it "should not be valid" do
-      user = FactoryGirl.build(:user, email: nil)
+      user = build(:user, email: nil)
       expect(user).to_not be_valid
     end
 
