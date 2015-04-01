79e941854d2af436732316c83874bc0640fc24ab
Andy Dau <tdau@colgate.edu>
2015-04-01 16:29:42 -0400

    add devise callback code

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,12 @@ class User < ActiveRecord::Base
     return @user
   end
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.name = auth.info.name # assuming the user model has a name
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+
 end
