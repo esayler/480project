f114e046c123d0280153b073fddc490821a52ba7
Eric Sayler <esayler@colgate.edu>
2015-04-03 14:01:13 -0400

    ignore old routes, update devise init config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -233,7 +233,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :google_oauth2, Rails.application.secrets.omniauth_provider_key, Rails.application.secrets.omniauth_provider_secret, { }
+  config.omniauth :google_oauth2, Rails.application.secrets.omniauth_provider_key, Rails.application.secrets.omniauth_provider_secret
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   resources :users
   root to: 'visitors#index'
-  get '/auth/:provider/callback' => 'sessions#create'
-  get '/signin' => 'sessions#new', :as => :signin
-  get '/signout' => 'sessions#destroy', :as => :signout
-  get '/auth/failure' => 'sessions#failure'
+  #get '/auth/:provider/callback' => 'sessions#create'
+  #get '/signin' => 'sessions#new', :as => :signin
+  #get '/signout' => 'sessions#destroy', :as => :signout
+  #get '/auth/failure' => 'sessions#failure'
 end
