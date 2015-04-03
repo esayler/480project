1c03a14e571aa2a3cc4835f9fd33ed48808f8ea3
Andy Dau <tdau@colgate.edu>
2015-04-02 21:10:24 -0400

    reroute to user creation on first time signin

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,14 +1,15 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
       # You need to implement the method below in your model (e.g. app/models/user.rb)
-      @user = User.from_omniauth(request.env["omniauth.auth"])
-      if @user.persisted?
+      auth = request.env["omniauth.auth"]
+      @user = User.where(provider: auth.provider, uid: auth.uid).first
+      if !(@user==nil)
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
         session[:user_id]=@user.id
         sign_in_and_redirect @user, :event => :authentication
       else
         session["devise.google_data"] = request.env["omniauth.auth"]
-        redirect_to new_user_registration_url
+        redirect_to new_user_path
       end
   end
 end
