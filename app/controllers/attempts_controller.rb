2b1397bf26a3d98eaed2e2776aba49a118050730
akminella <akminella@colgate.edu>
2015-04-13 21:03:09 -0400

    Create attempt submission form and add methods to controller

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -1,6 +1,31 @@
 class AttemptsController < ApplicationController
-
+        before_filter :authenticate_user!
 	def index
-		@attempts = Attempt.all
+	    @attempts = Attempt.all
 	end
+
+        def show
+            @attempt = Attempt.find(params[:id])
+        end
+
+        def new
+        end
+
+        def create
+            a = Attempt.new(create_params)
+            a.user_id = current_user_id
+            if a.save
+                flash[:notice] = "Attempt submitted successfully!"
+                redirect_to problems_path
+            else
+                flash[:warning] = "Attempt couldn't be submitted"
+                redirect_to new_attempt_path
+            end
+        end
+
+private
+        def create_params
+            params.require(:submission)
+        end
+            
 end
