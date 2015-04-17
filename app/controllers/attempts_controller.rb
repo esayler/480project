5e4b0d6aef7ae6720dd4943208b8719b973fe047
C.J. Jennings <cfjennings@gmail.com>
2015-04-17 16:06:27 -0400

    Reformat view for Attempts#show

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -1,35 +1,35 @@
 class AttemptsController < ApplicationController
-        before_filter :authenticate_user!
+    before_filter :authenticate_user!
 	def index
 	    @attempts = Attempt.all
 	end
 
-        def show
-            @attempt = Attempt.find(params[:id])
-        end
+    def show
+        @attempt = Attempt.find(params[:id])
+    end
 
-        def new
-        	@problem = Problem.find(params[:problem_id])
-        	@attempt = Attempt.new()
-        end
+    def new
+    	@problem = Problem.find(params[:problem_id])
+    	@attempt = Attempt.new()
+    end
 
-        def create
-            a = Attempt.new(create_params)
-            a.user_id = current_user.id
-        	@problem = Problem.find(params[:problem_id])
-        	a.problem_id = @problem.id
-            if a.save
-                flash[:notice] = "Attempt submitted successfully!"
-                redirect_to problem_attempts_path(@problem.id)
-            else
-                flash[:warning] = "Attempt couldn't be submitted"
-                redirect_to new_problem_attempt_path(@problem.id)
-            end
+    def create
+        a = Attempt.new(create_params)
+        a.user_id = current_user.id
+    	@problem = Problem.find(params[:problem_id])
+    	a.problem_id = @problem.id
+        if a.save
+            flash[:notice] = "Attempt submitted successfully!"
+            redirect_to problem_attempts_path(@problem.id)
+        else
+            flash[:warning] = "Attempt couldn't be submitted"
+            redirect_to new_problem_attempt_path(@problem.id)
         end
+    end
 
 private
-        def create_params
-            params.require(:attempt).permit(:submission)
-        end
+    def create_params
+        params.require(:attempt).permit(:submission)
+    end
             
 end
