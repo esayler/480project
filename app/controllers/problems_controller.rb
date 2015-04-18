80eccf4970f5527cb20c9f5872318dd1b18036cf
Eric Sayler <esayler@colgate.edu>
2015-04-18 00:43:38 -0400

    update create problem method

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,33 +1,31 @@
 class ProblemsController < ApplicationController
-	before_filter :authenticate_user!
-	def index
-    	@problems = Problem.all
-  	end
+  before_filter :authenticate_user!
+  def index
+    @problems = Problem.all
+  end
 
-  	def show
-    	@problem = Problem.find(params[:id])
-  	end
+  def show
+    @problem = Problem.find(params[:id])
+  end
 
-  	def new
-  		@problem = Problem.new
-  	end
+  def new
+    @problem = Problem.new
+  end
 
-  	def create
-		p = Problem.new(create_params)
-		p.user_id = current_user.id
-		if p.save
-			flash[:notice] = "New problem #{p.name} created successfully"
-			redirect_to problems_path
-		else
-			flash[:warning] = "Problem couldn't be created"
-			redirect_to new_problem_path
-		end
-	end
+  def create
+    if @problem = current_user.problems.create(create_params)
+      flash[:notice] = "New problem #{@problem.name} created successfully"
+      redirect_to problems_path
+    else
+      flash[:warning] = "Problem couldn't be created"
+      redirect_to new_problem_path
+    end
+  end
 
-private
-	def create_params
-		params.require(:problem).permit(:name, :description, :difficulty)
-	end
+  private
+  def create_params
+    params.require(:problem).permit(:name, :description, :difficulty)
+  end
 
 end
 
