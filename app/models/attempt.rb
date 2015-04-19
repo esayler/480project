139f94406f635b89f264a916a9f3978a915290d7
Eric Sayler <esayler@colgate.edu>
2015-04-19 14:40:13 -0400

    add enum role to User model, clean up other models

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -1,14 +1,16 @@
 class Attempt < ActiveRecord::Base
+
     belongs_to :user
     belongs_to :problem
 
     validates :user_id, :problem_id, :submission, presence: true
 
     def show_grade
-    	if self.grade == -1
-    		"Not yet graded"
-    	else
-    		self.grade.to_s
-    	end
+      if self.grade == -1
+        "Not yet graded"
+      else
+        self.grade.to_s
+      end
     end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ActiveRecord::Base
 
   has_many :problems
   has_many :attempts
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:google_oauth2]
+
   validates :name, :email, presence: true
   validates :name, :email, uniqueness: true
+
+  enum role: [:student, :alum, :prof, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -16,10 +16,23 @@ class User < ActiveRecord::Base
       user.name = auth.info.name # assuming the user model has a name
       user.password = Devise.friendly_token[0,20]
     end
+
   end
-  
+
+
   def password_required?
     super && self.provider.blank?
   end
+
+
+  def set_default_role
+    self.role ||= :student
+  end
+
+
+  devise :database_authenticatable, :registerable,
+        #:confirmable, :lockable, :timeoutable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:google_oauth2]
 
 end
