cfe768b68e4eb9acb47bd0e094211b7e76607cf7
Eric Sayler <esayler@colgate.edu>
2015-04-28 18:34:44 -0400

    switch to minifid version of ACE and add indent guides to ACE startup

--- app/assets/javascripts/attempts.js
+++ app/assets/javascripts/attempts.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
-	var minute = $('.temp_information').data('minute')
-	var oldDateObj = Date.now()
-	var newTime = new Date(oldDateObj + minute*60000);
-	console.log(minute);
-	console.log(newTime)
-	$('#until2d').countdown({until: newTime});
+    var minute = $('.temp_information').data('minute')
+    var oldDateObj = Date.now()
+    var newTime = new Date(oldDateObj + minute*60000);
+    console.log(minute);
+    console.log(newTime)
+    $('#until2d').countdown({until: newTime});
 });
 
 
 $(document).ready(function() {
     var editor = ace.edit("editor");
-editor.setTheme("ace/theme/tomorrow");
-editor.getSession().setMode("ace/mode/python");
+    editor.setTheme("ace/theme/tomorrow");
+    editor.setDisplayIndentGuides(true)
+    editor.getSession().setMode("ace/mode/python");
 });
