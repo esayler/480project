ad3da55bd1e9edc66caf0f0f40298de1211cc542
Andy Dau <tdau@colgate.edu>
2015-05-01 21:09:16 -0400

    add more user tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,8 +1,27 @@
-describe UsersController, :skip => "not yet implemented" do
+describe UsersController do
 
-  #before do
-    #request.env["devise.mapping"] = Devise.mappings[:user] # If using Devise
-    #request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2] 
-  #end
+  before :each do
 
+    #let (:user) { FactoryGirl.create :user, :student }
+
+    @user = create(:user, id: 1, role: 2)
+    # @attempt1 = create(:attempt, problem_id: 1)
+    # @attempt2 = create(:attempt, problem_id: 1)
+    allow(controller).to receive(:authenticate_user!).and_return(true)
+    allow(controller).to receive(:current_user){ @user }
+  end
+
+  describe "GET #show" do
+    it "routes correctly" do
+      get :show, id: @user.id
+      #expect(Problem).to receive(:find).with("1") { @problem1 }
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the show template" do
+      get :show, id: @user.id
+      #expect(Problem).to receive(:find).with("1") { @problem1 }
+      expect(response).to render_template(:show)
+    end
+  end
 end
